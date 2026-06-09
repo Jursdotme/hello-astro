a8cd722bf2980565bfada16eaa33d8f0d8fe3398
jursdotme <rasmus@jurs.me>
2026-06-09 15:39:01 +0200

    Update site URL and base path in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jursdotme.github.io/helloastro',
-  base: '/helloastro',
+  site: 'https://demo.jurs.me',
+  base: '/',
 });
